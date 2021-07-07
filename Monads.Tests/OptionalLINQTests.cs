15d8c6a076b25e719b5e93e64ffc2a4c614fdc55
Rajiv Singh <rajivonline@gmail.com>
2021-07-08 00:12:05 +0530

    Optional<T> async select queries now use Task extensions instead of async overloads of own select methods; Unit tests updated to show appropriate usage

--- Monads.Tests/OptionalLINQTests.cs
+++ Monads.Tests/OptionalLINQTests.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS8848
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -20,34 +21,12 @@ namespace HonestyDotNet.Monads.Tests
                         select s.Length;
 
             Assert.True(ol1.IsSome);
-            Assert.Equal(hw.Length, ol1.Value);                        
+            Assert.Equal(hw.Length, ol1.Value);
             Assert.False(ol2.IsSome);
         }
 
         [Fact]
-        public void Optional_SelectManyT()
-        {
-            var h = "Hello ";
-            var w = "World!";
-            var oh = Optional.Try(() => h);
-            var ow = Optional.Try(() => w);
-            var ox = Optional<string>.None;
-
-            var ohw =   from s1 in oh
-                        from s2 in ow
-                        select s1 + s2;
-
-            var ohx =   from s1 in oh
-                        from s2 in ox
-                        select s1 + s2;
-
-            Assert.True(ohw.IsSome);
-            Assert.Equal(h + w, ohw.Value);
-            Assert.False(ohx.IsSome);            
-        }
-
-        [Fact]
-        public void Optional_SelectManyT1T2()
+        public void Optional_SelectMany()
         {
             var h = "High ";
             var f = 5;
@@ -65,7 +44,7 @@ namespace HonestyDotNet.Monads.Tests
 
             Assert.True(ohw.IsSome);
             Assert.Equal(h + f, ohw.Value);
-            Assert.False(ohx.IsSome);            
+            Assert.False(ohx.IsSome);
         }
 
         [Fact]
@@ -76,34 +55,61 @@ namespace HonestyDotNet.Monads.Tests
 
             async Task<int> AsyncCodeOf(string i) => await Task.Run(() 
                 => { return (int) Math.Sqrt(i.GetHashCode()); });
-
-            async Task<int> AsyncSquare(int i) => await Task.Run(() => { return i * i; });
             
             var r1 = await 
-                    (from v in await Optional.Try(AsyncCodeOf, sHello)
-                     select AsyncSquare(v));
+                     from maybeValue in Optional.Try(AsyncCodeOf, sHello)
+                     select
+                     (
+                         from value in maybeValue
+                         select value * value
+                     );
+            Assert.True(r1.IsSome);
 
             var r2 = await 
-                    (from v in await Optional.Try(AsyncCodeOf, sNull)
-                     select AsyncSquare(v));                     
-            
-            Assert.True(r1.IsSome);
+                     from maybeValue in Optional.Try(AsyncCodeOf, sNull)
+                     select
+                     (
+                         from value in maybeValue
+                         select value * value
+                     );
             Assert.False(r2.IsSome);
         }
 
         [Fact]
         public async Task Optional_SelectManyAsyc()
         {
-            var o1 = Optional.Some(10);
-            async Task<Optional<string>> ComputeString(int x) => await Task.Run(() => { return x.ToString(); });
-            async Task<bool> LengthCheck(int a, string s) => await Task.Run(() => a + s.Length > 7);
+            var sHello = "Hello";
+            var sWorld = "World";
+            string sNull = null;
 
-            var r1 = await
-                     (from v1 in o1
-                     from v2 in ComputeString(v1)
-                     select LengthCheck(v1, v2));
+            async Task<int> AsyncCodeOf(string i) => await Task.Run(() 
+                => { return (int) Math.Sqrt(i.GetHashCode()); });
+            
+            var r1 = await 
+                     from maybeValue1 in Optional.Try(AsyncCodeOf, sHello)
+                     from maybeValue2 in Optional.Try(AsyncCodeOf, sWorld)
+                     from maybeValue3 in Optional.Try(AsyncCodeOf, sHello + sWorld)
+                     select
+                     (
+                         from value1 in maybeValue1
+                         from value2 in maybeValue2
+                         from value3 in maybeValue3
+                         select value1 + value2 + value3
+                     );
             Assert.True(r1.IsSome);
-            Assert.True(r1.Value);
+
+            var r2 = await 
+                     from maybeValue1 in Optional.Try(AsyncCodeOf, sHello)
+                     from maybeValue2 in Optional.Try(AsyncCodeOf, sNull)
+                     from maybeValue3 in Optional.Try(AsyncCodeOf, sHello + sWorld)
+                     select
+                     (
+                         from value1 in maybeValue1
+                         from value2 in maybeValue2
+                         from value3 in maybeValue3
+                         select value1 + value2 + value3
+                     );
+            Assert.False(r2.IsSome);
         }
     }
 }
--- Monads/OptionalLINQ.cs
+++ Monads/OptionalLINQ.cs
@@ -7,12 +7,7 @@ namespace HonestyDotNet.Monads
     {
         public static Optional<T2> Select<T1, T2>(this Optional<T1> o, Func<T1, T2> f) => o.Map(f);
 
-        public static async Task<Optional<T2>> Select<T1, T2>(this Optional<T1> o, Func<T1, Task<T2>> f) => await o.Map(f);
-
         public static Optional<T3> SelectMany<T1, T2, T3>(this Optional<T1> o, Func<T1, Optional<T2>> f, Func<T1, T2, T3> g) 
             => o.Bind(v1 => f(v1).Bind(v2 => g(v1, v2).ToOptional()));
-
-        public static async Task<Optional<T3>> SelectMany<T1, T2, T3>(this Optional<T1> o, Func<T1, Task<Optional<T2>>> f, Func<T1, T2, Task<T3>> g) 
-            => await o.Bind(async v1 => await (await f(v1)).Bind(async v2 => (await g(v1, v2)).ToOptional()));
     }
 }
