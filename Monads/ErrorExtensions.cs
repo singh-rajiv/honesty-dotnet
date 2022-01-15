e02fa05a1f1cda765e697dac35e5bc8e8eb23639
Rajiv Singh <rajivonline@gmail.com>
2022-01-15 16:33:23 +0530

    Generate Documentation File for nuget package

--- Monads/ErrorExtensions.cs
+++ Monads/ErrorExtensions.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS1591
 namespace Monads;
 
 public static class ErrorExtensions
--- Monads/Unit.cs
+++ Monads/Unit.cs
@@ -1,5 +1,8 @@
 namespace Monads;
 
+/// <summary>
+/// Type in FP that represents absense of a specific value. Resembles the void type in OOP.
+/// </summary>
 public struct Unit
 {
 
