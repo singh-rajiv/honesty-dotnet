42474f4a137561cc20490ae059bdc07ef5e86a16
Rajiv Singh <rajivonline@gmail.com>
2022-02-21 00:29:52 +0530

    Nullable reference type migration

--- Monads/ErrorLINQ.cs
+++ Monads/ErrorLINQ.cs
@@ -10,11 +10,11 @@ public static class ErrorLINQ
     /// <summary>
     /// Not to be called directly. Use Map if using method syntax.
     /// </summary>
-    public static Error<T2> Select<T1, T2>(this Error<T1> e, Func<T1, T2> f) => e.Map(f);
+    public static Error<T2> Select<T1, T2>(this Error<T1> e, Func<T1, T2?> f) => e.Map(f);
 
     /// <summary>
     /// Not to be called directly. Use Bind if using method syntax.
     /// </summary>
-    public static Error<T3> SelectMany<T1, T2, T3>(this Error<T1> e, Func<T1, Error<T2>> f, Func<T1, T2, T3> g)
+    public static Error<T3> SelectMany<T1, T2, T3>(this Error<T1> e, Func<T1, Error<T2>> f, Func<T1, T2, T3?> g)
         => e.Bind(v1 => f(v1).Bind(v2 => g(v1, v2).ToError()));
 }
